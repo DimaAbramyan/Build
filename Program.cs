ac8b0f0f680c4cafe67637f46662590b87ac0839
DimaAbramyan <146520944+DimaAbramyan@users.noreply.github.com>
2024-12-27 19:51:30 +0300

    AddedMain

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+
+class Program
+{
+    int Func(int x)
+    {
+        return x * x - x;
+    }
+    void Main(string[] args)
+    {
+        int a = 4;
+        Debug.Assert(Func(a) == 12);
+        a = 10;
+        Debug.Assert(Func(a) == 90);
+        a = 2;
+        Debug.Assert(Func(a) == 2);
+    }
+}
